6e775f748dd64d9309e8cbe707b1804939752ab0
MIGUEL ANGEL RUIZ HERNANDEZ <miguel.ruiz-hernandez@siemens.com>
2024-04-08 20:52:59 -0600

    Consumir el repositorio directamente

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,17 +1,22 @@
 import { Request, Response} from 'express';
 import { RegisterUserDto } from '../../domain';
+import { AuthRepository } from '../../domain/repositories/auth.repository';
 
 export class AuthController {
 
     // DI
-    constructor() {}
+    constructor(
+        private readonly authRepository: AuthRepository, 
+    ) {}
 
     registerUser = (req: Request, res: Response) => {
 
         const [ error, registerUserDto ] = RegisterUserDto.create(req.body);
         if (error) return res.status(400).json( error );
 
-        res.json( registerUserDto );
+        this.authRepository.register( registerUserDto! )
+            .then( user => res.json( user ) )
+            .catch( error => res.status(500).json(error) );
     }
 
     loginUser = (req: Request, res: Response) => {
